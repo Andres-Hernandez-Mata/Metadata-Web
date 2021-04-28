133ae2e472c7d880ab7e66d2d6475994fcdf8111
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-28 14:22:24 -0500

    Agregar argumento
    Link de descarga

--- src/metadata.py
+++ src/metadata.py
@@ -6,3 +6,19 @@ Python: 3.9.1
 Fecha: 26 Abril 2020
 """
 
+import argparse
+import os
+import time
+from datetime import datetime
+import time
+
+os.system("cls")
+
+descripion = "Example: -l www.google.com"
+parser = argparse.ArgumentParser(description='Metadata', epilog=descripion, formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument("-l,", metavar='--Link', dest="link", help="Link para descargar imagenes", required=True)
+
+params = parser.parse_args()
+
+print("[INFO] Buscando...", params.link)
+
